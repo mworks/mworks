358511f3b6011d88256ef32b1bcd01ffb4b4d06c
David Cox <david.daniel.cox@gmail.com>
2009-01-23 11:46:38 -0500

    Removed build tree from version control

--- build/MonkeyWorksCocoa.build/Debug/MonkeyWorksCocoa.build/Script-DA57CD5D083A6378006832CF.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/tcsh
-# copy the framework to the proper directory
-rm -dfR "/Library/Frameworks/MonkeyWorksCocoa.framework"
-cp -Rf "$BUILT_PRODUCTS_DIR/MonkeyWorksCocoa.framework" "/Library/Frameworks"
-
