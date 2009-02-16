50e73295a6634512fe8eb37179b2c907533ba2d7
David Cox <david.daniel.cox@gmail.com>
2009-02-16 16:17:14 -0500

    Minor fixes, still in pursuit of working buildbot

--- buildbot_master/start_buildbot_master.sh
+++ buildbot_master/start_buildbot_master.sh
@@ -12,5 +12,6 @@ touch $MASTER_DIRECTORY/twistd.log
 buildbot create-master $MASTER_DIRECTORY
 cp ./master.cfg $MASTER_DIRECTORY
 cp -r ./plugins $MASTER_DIRECTORY/
+cp ./buildbot.css $MASTER_DIRECTORY/public_html/
 
 buildbot start $MASTER_DIRECTORY
