a93a6205286001363fa606ae13bb0687aa9bd6eb
David Cox <david.daniel.cox@gmail.com>
2009-02-16 16:38:14 -0500

    Seems that the installer builds and uploads correctly now... plus, nicer css for the webpage

--- buildbot_master/start_buildbot_master.sh
+++ buildbot_master/start_buildbot_master.sh
@@ -12,6 +12,7 @@ touch $MASTER_DIRECTORY/twistd.log
 buildbot create-master $MASTER_DIRECTORY
 cp ./master.cfg $MASTER_DIRECTORY
 cp -r ./plugins $MASTER_DIRECTORY/
-cp ./buildbot.css $MASTER_DIRECTORY/public_html/
+cp ./*.css $MASTER_DIRECTORY/public_html/
+cp ./*.html $MASTER_DIRECTORY/public_html/
 
 buildbot start $MASTER_DIRECTORY
