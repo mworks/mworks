21e8aa77abeb3a318e097c732ae0c1b0d421cd2a
David Cox <david.daniel.cox@gmail.com>
2009-02-17 10:03:28 -0500

    Allow buildbot_master start script to be called from any directory

--- buildbot_master/start_buildbot_master.sh
+++ buildbot_master/start_buildbot_master.sh
@@ -5,8 +5,10 @@
 # on networked build "slave" instances
 
 MASTER_DIRECTORY=~/.buildbot_master
+SCRIPT_DIRECTORY=`dirname $0`
 
-if ps -e | grep "\.buildbot_master" | grep -v "grep";
+
+if ps -e | grep "\.buildbot_master" | grep -v "grep"; # NB: this signature must match MASTER_DIRECTORY, above
 then
     echo "Buildbot master already started"
     exit
@@ -16,9 +18,9 @@ rm -rf $MASTER_DIRECTORY
 mkdir -p $MASTER_DIRECTORY
 touch $MASTER_DIRECTORY/twistd.log
 buildbot create-master $MASTER_DIRECTORY
-cp ./master.cfg $MASTER_DIRECTORY
-cp -r ./plugins $MASTER_DIRECTORY/
-cp ./*.css $MASTER_DIRECTORY/public_html/
-cp ./*.html $MASTER_DIRECTORY/public_html/
+cp $SCRIPT_DIRECTORY/master.cfg $MASTER_DIRECTORY
+cp -r $SCRIPT_DIRECTORY/plugins $MASTER_DIRECTORY/
+cp $SCRIPT_DIRECTORY/*.css $MASTER_DIRECTORY/public_html/
+cp $SCRIPT_DIRECTORY/*.html $MASTER_DIRECTORY/public_html/
 
 buildbot start $MASTER_DIRECTORY
