2bec4f6dee15dde2685218e080d388a02cc97050
David Cox <david.daniel.cox@gmail.com>
2009-11-21 10:02:00 -0500

    Missing file initial commit

---
+++ VariableRegistryInterface.cpp
@@ -0,0 +1,61 @@
+/*
+ *  VariableRegistryInterface.cpp
+ *  MonkeyWorksCore
+ *
+ *  Created by David Cox on 11/19/09.
+ *  Copyright 2009 Harvard University. All rights reserved.
+ *
+ */
+
+#include "VariableRegistryInterface.h"
+
+using namespace mw;
+
+shared_ptr<Variable> VariableRegistryInterface::getVariable(const std::string &tag) {
+	if(registry != NULL){
+        return registry->getVariable(tag);
+    } else {
+        return shared_ptr<Variable>();
+    }
+}
+
+shared_ptr<Variable> VariableRegistryInterface::getVariable(const int code) {
+	if(registry != NULL){
+        return registry->getVariable(code);
+    } else {
+        return shared_ptr<Variable>();
+    }
+}
+
+int VariableRegistryInterface::lookupCodeForTag(const std::string &tag) {
+	shared_ptr<Variable> var = getVariable(tag);
+	if(var) {
+		return var->getCodecCode();
+	} else {
+		return -1;
+	}
+}
+
+std::vector<std::string> VariableRegistryInterface::getVariableTagNames() {
+	if(registry != NULL){
+        return registry->getVariableTagNames();
+    } else {
+        return std::vector<std::string>();
+    }
+}
+
+
+RegistryAwareEventStreamInterface::RegistryAwareEventStreamInterface(MessageDomain dom, bool locking) : EventStreamInterface(dom, locking){ }
+
+void RegistryAwareEventStreamInterface::registerCallback(string tagname, EventCallback cb, string callback_key){
+    int code = lookupCodeForTag(tagname);   
+    EventStreamInterface::registerCallback(code, cb, callback_key);
+}
+
+void RegistryAwareEventStreamInterface::registerCallback(EventCallback cb, string callback_key){
+    EventStreamInterface::registerCallback(cb, callback_key);
+}
+
+void RegistryAwareEventStreamInterface::registerCallback(int code, EventCallback cb, string callback_key){
+    EventStreamInterface::registerCallback(code, cb, callback_key);
+}
