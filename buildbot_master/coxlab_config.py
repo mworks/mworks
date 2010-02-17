b8511b994dad8d45bceea1eda950a4c08dc35b3d
Christopher Stawarz <cstawarz@mit.edu>
2010-02-17 16:50:29 -0500

    More master.cfg updates

--- buildbot_master/coxlab_config.py
+++ buildbot_master/coxlab_config.py
@@ -1,3 +1,4 @@
 repository_base_url = 'git://github.com/monkeyworks-project/' 
 installer_destination_path = '/home/coxlab/webapps/public/'
+project_url = 'http://monkeyworks-project.github.com'
 buildbot_hostname = 'monkeyworks.coxlab.org'
--- buildbot_master/dicarlolab_config.py
+++ buildbot_master/dicarlolab_config.py
@@ -1,3 +1,4 @@
 repository_base_url = 'git://github.com/dicarlolab-monkeyworks/' 
 installer_destination_path = '/Library/WebServer/Documents/mw/'
+project_url = 'http://github.com/dicarlolab-monkeyworks'
 buildbot_hostname = 'dicarlo-mwdev.mit.edu'
