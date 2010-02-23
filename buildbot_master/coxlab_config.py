5d0c79c65f1f610df347de74d3a1fcd8107698b3
Christopher Stawarz <cstawarz@mit.edu>
2010-02-23 12:28:09 -0500

    Updated GitHub URLs

--- buildbot_master/coxlab_config.py
+++ buildbot_master/coxlab_config.py
@@ -1,4 +1,4 @@
-repository_base_url = 'git://github.com/monkeyworks-project/' 
+repository_base_url = 'git://github.com/mworks-project/' 
 installer_destination_path = '/home/coxlab/webapps/public/'
 project_url = 'http://monkeyworks-project.github.com'
 buildbot_hostname = 'monkeyworks.coxlab.org'
--- buildbot_master/dicarlolab_config.py
+++ buildbot_master/dicarlolab_config.py
@@ -1,4 +1,4 @@
-repository_base_url = 'git://github.com/dicarlolab-monkeyworks/' 
+repository_base_url = 'git://github.com/dicarlolab-mworks/' 
 installer_destination_path = '/Library/WebServer/Documents/mw/'
-project_url = 'http://github.com/dicarlolab-monkeyworks'
+project_url = 'http://github.com/dicarlolab-mworks'
 buildbot_hostname = 'dicarlo-mwdev.mit.edu'
