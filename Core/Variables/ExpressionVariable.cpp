c4f321f0ffb1d92fe8ca4456c60cf778819b83e8
Christopher Stawarz <cstawarz@mit.edu>
2010-03-24 18:30:17 -0400

    Disabled ParsedExpressionVariable debugging output

--- Core/Variables/ExpressionVariable.cpp
+++ Core/Variables/ExpressionVariable.cpp
@@ -150,12 +150,12 @@ ParsedExpressionVariable::ParsedExpressionVariable(string _expression_string) : 
 	expression_string = boost::algorithm::trim_copy(dest_string); 
 	dest_string = "";
 	
-	cerr << "expression: " << expression_string << endl;
+	//cerr << "expression: " << expression_string << endl;
 	//cerr << "dest string: " << dest_string << endl;
 	
 	try {
 		expression_tree = stx::parseExpression(expression_string);
-		std::cerr << "parsed: " << expression_tree.toString() << "\n";
+		//std::cerr << "parsed: " << expression_tree.toString() << "\n";
 	} catch (stx::ExpressionParserException &e){
 		std::cerr << "ExpressionParserException: " << e.what() << "\n";
 		throw e;
