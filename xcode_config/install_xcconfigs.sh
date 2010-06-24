86c951f96c93274ec7303f236ddbcd380033624f
Christopher Stawarz <cstawarz@mit.edu>
2010-06-24 18:43:46 -0400

    Added a Makefile to xcode_config.  mw_xcodebuild is now installed in Developer/bin.

--- xcode_config/install_xcconfigs.sh
+++ xcode_config/install_xcconfigs.sh
@@ -3,4 +3,4 @@
 APP_SUPPORT_DIR="/Library/Application Support/MWorks/Developer/Xcode/"
 
 mkdir -p "$APP_SUPPORT_DIR"
-cp *.xcconfig WARNING.txt mw_xcodebuild "$APP_SUPPORT_DIR"
+cp *.xcconfig WARNING.txt "$APP_SUPPORT_DIR"
