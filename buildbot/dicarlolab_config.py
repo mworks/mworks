8bec5da178a6a5cc627e31a8db6f13e98fdc754d
Christopher Stawarz <cstawarz@mit.edu>
2010-12-08 10:37:12 -0500

    Renamed buildbot passwords file

--- buildbot/dicarlolab_config.py
+++ buildbot/dicarlolab_config.py
@@ -5,8 +5,7 @@ buildbot_hostname = 'dicarlo-mwdev.mit.edu'
 buildbot_admin = 'Christopher Stawarz <cstawarz@mit.edu>'
 webstatus_port = 8010
 
-from slave_pass import slave_pass, webstatus_pass
+from local_passwords import slave_pass, webstatus_pass
 slave_port = 9989
 
 installer_destination_path = '/Library/WebServer/Documents/mw'
-installer_download_base_url = 'http://dicarlo-mwdev.mit.edu/mw'
