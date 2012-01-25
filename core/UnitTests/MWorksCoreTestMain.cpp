4709aada35466eb2030a52cc192ffc313353f290
Christopher Stawarz <cstawarz@mit.edu>
2012-01-25 15:41:19 -0500

    Moved the main function for MWorksCoreTestRunner into MWorksCoreTest.framework, which allows CppUnit to catch its own exceptions (and thereby provide test failure details).  Moved some common build settings to Development.xcconfig.  Cleaned up the Xcode project files for Scarab and MWorksCore (removed unused targets and configurations and redundant or unneeded settings).

---
+++ core/UnitTests/MWorksCoreTestMain.cpp
@@ -0,0 +1,77 @@
+//
+//  MWorksCoreTestMain.cpp
+//  MWorksCore
+//
+//  Created by Christopher Stawarz on 1/25/12.
+//  Copyright (c) 2012 MIT. All rights reserved.
+//
+
+#include "MWorksCoreTestMain.h"
+
+#include <cppunit/TextTestRunner.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/TestCase.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/TestFailure.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/XmlOutputter.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+
+class MWorksTestProgressListener : public CppUnit::BriefTestProgressListener {
+    
+public:
+    void addFailure(const CppUnit::TestFailure &failure) {
+        CppUnit::BriefTestProgressListener::addFailure(failure);
+        
+        CppUnit::Exception *exception = failure.thrownException();
+        std::cout << std::endl << std::endl;
+        
+        CppUnit::SourceLine sourceLine = exception->sourceLine();
+        if (sourceLine.isValid()) {
+            std::cout << "File: " << sourceLine.fileName() << std::endl
+                      << "Line: " << sourceLine.lineNumber() << std::endl;
+        }
+        
+        std::cout << "Message: " << exception->what();
+    }
+    
+};
+
+
+int MWorksCoreTestMain(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " results_file [test_name ...]" << std::endl;
+        return 2;
+    }
+    
+    CppUnit::TextTestRunner runner;
+    
+    std::ofstream outfile( argv[1] );
+    CppUnit::XmlOutputter *outputter = new CppUnit::XmlOutputter(&runner.result(), outfile);
+    runner.setOutputter(outputter);
+    
+    MWorksTestProgressListener listener;
+    runner.eventManager().addListener( &listener );
+    
+    CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry("Unit Test");
+    runner.addTest( registry.makeTest() );
+    
+    bool returnval = true;
+    if (argc > 2) {
+        // Run specified tests
+        for (int i = 2; i < argc; i++) {
+            returnval = runner.run( argv[i], false, false, false ) && returnval;
+        }
+    } else {
+        // Run all tests
+        returnval = runner.run( "", false, false, false );
+    }
+    
+    outputter->write();
+    outfile.close();
+    
+    // return !returnval because it is 1 on success
+    return !returnval;
+}
