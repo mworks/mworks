3ef6d750b1733255eed575239a9a1eeb32c54556
Christopher Stawarz <cstawarz@mit.edu>
2012-05-30 11:12:18 -0400

    MWClient's pause/resume button now changes appearance when the experiment is paused

---
+++ apps/client/MWExperimentPauseResumeIconTransformer.h
@@ -0,0 +1,14 @@
+//
+//  MWExperimentPauseResumeIconTransformer.h
+//  MWClient
+//
+//  Created by Christopher Stawarz on 5/30/12.
+//  Copyright (c) 2012 MIT. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface MWExperimentPauseResumeIconTransformer : NSValueTransformer
+
+@end
