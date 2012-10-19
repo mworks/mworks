6c36228025e93d3aadb92ceaaae4b2e56a931f1b
Christopher Stawarz <cstawarz@mit.edu>
2012-10-19 11:40:32 -0400

    Removed some obsolete build scripts and related files

--- build/xcode_config/install_xcconfigs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-APP_SUPPORT_DIR="/Library/Application Support/MWorks/Developer/Xcode/"
-
-mkdir -p "$APP_SUPPORT_DIR"
-cp *.xcconfig WARNING.txt "$APP_SUPPORT_DIR"
--- build/xcode_config/pull_xcconfigs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-APP_SUPPORT_DIR="/Library/Application Support/MWorks/Developer/Xcode"
-
-cp "$APP_SUPPORT_DIR"/*.xcconfig ./
-cp "$APP_SUPPORT_DIR"/WARNING.txt ./
-
