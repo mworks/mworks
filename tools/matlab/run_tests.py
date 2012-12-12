ca9099a08bf8e34761d6ab0eb1e942ac44805b83
Christopher Stawarz <cstawarz@mit.edu>
2012-12-12 13:05:23 -0500

    Started work on MATLAB tools test suite

--- tools/matlab/run_tests.py
+++ tools/matlab/run_tests.py
@@ -1,5 +1,5 @@
 import os
-from StringIO import StringIO
+import shutil
 import subprocess
 import sys
 import warnings
@@ -34,18 +34,55 @@ quit(~passed)
 ''' % (mw_developer_dir, mw_matlab_dir)
 
 
+def create_test_file():
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore', RuntimeWarning)
+        filename = os.tempnam()
+
+    with MWKStream._create_file(filename) as fp:
+        code = 1
+        tagmap = {}
+
+        for tag, data in (
+                ('undefined', None),
+                ('int_zero', 0),
+                ('int_pos', 1),
+                ('int_neg', -2),
+                ):
+            tagmap[tag] = code
+            fp._write([code, code, data])
+            code += 1
+
+        fp._write([0, 0, tagmap])
+
+    return filename
+
+
+def remove_test_file(filename):
+    if os.path.isdir(filename):
+        shutil.rmtree(filename)
+    else:
+        os.remove(filename)
+
+
 def run_matlab(arch):
-    args = (
-        '/Applications/MATLAB_%s.app/bin/matlab' % mw_matlab_version,
-        '-' + {'i386': 'maci', 'x86_64': 'maci64'}[arch],
-        '-nodisplay',
-        '-nojvm',
-        )
-    cmd = subprocess.Popen(args=args, stdin=subprocess.PIPE)
-    cmd.stdin.write(matlab_script)
-    cmd.wait()
-    print  # Add a newline to the output
-    return cmd.returncode
+    filename = create_test_file()
+    try:
+        env = os.environ.copy()
+        env['MW_MATLAB_TEST_FILENAME'] = filename
+        args = (
+            '/Applications/MATLAB_%s.app/bin/matlab' % mw_matlab_version,
+            '-' + {'i386': 'maci', 'x86_64': 'maci64'}[arch],
+            '-nodisplay',
+            '-nojvm',
+            )
+        cmd = subprocess.Popen(args=args, stdin=subprocess.PIPE, env=env)
+        cmd.stdin.write(matlab_script)
+        cmd.wait()
+        print  # Add a newline to the output
+        return cmd.returncode
+    finally:
+        remove_test_file(filename)
 
 
 def main():
--- tools/matlab/tests/testTypeConversion.m
+++ tools/matlab/tests/testTypeConversion.m
@@ -1,5 +1,29 @@
 function test_suite = testTypeConversion
 initTestSuite;
 
-function testNothing
-assertTrue(true);
+function assertInteger(expected, actual)
+assertTrue(isa(actual, 'int64'));
+assertEqual(int64(expected), actual);
+
+function data = getData(code)
+if ischar(code)
+    tagmap = getData(0);
+    data = getData(double(getfield(tagmap, code)));
+else
+    e = getEvents(getenv('MW_MATLAB_TEST_FILENAME'), [code]);
+    assertTrue(isstruct(e));
+    assertTrue(isvector(e));
+    assertEqual(1, length(e));
+    assertEqual(3, length(fieldnames(e)));
+    assertInteger(code, e.event_code);
+    assertInteger(code, e.time_us);
+    data = e.data;
+end
+
+function testUndefined
+assertTrue(isempty(getData('undefined')));
+
+function testInteger
+assertInteger(0, getData('int_zero'));
+assertInteger(1, getData('int_pos'));
+assertInteger(-2, getData('int_neg'));
