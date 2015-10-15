2a0bb85a0ac6428193df4b34bb4ec44c4ac2a5fb
Christopher Stawarz <cstawarz@mit.edu>
2015-10-15 10:13:25 -0400

    Attempt to prevent Python tools tests from failing spuriously due to existing Boost.Interprocess-created files

--- tools/python/run_tests.py
+++ tools/python/run_tests.py
@@ -1,3 +1,5 @@
+from glob import iglob
+import os
 import sys
 try:
     import unittest2 as unittest
@@ -6,10 +8,18 @@ except ImportError:
 
 
 def main():
+    # Remove any files previously created by Boost.Interprocess, as these
+    # sometimes cause tests to fail spuriously
+    for path in iglob('/tmp/boost_interprocess/mworks_conduit_test_*'):
+        if os.path.isfile(path):
+            print >>sys.stderr, 'Removing', path
+            os.remove(path)
+
     if len(sys.argv) == 1:
         tests = unittest.defaultTestLoader.discover('.')
     else:
         tests = unittest.defaultTestLoader.loadTestsFromNames(sys.argv[1:])
+
     suite = unittest.TestSuite(tests)
     runner = unittest.TextTestRunner()
     result = runner.run(suite)
