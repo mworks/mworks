mworks/mworks

commit b6dfa1f04e9ad03d140a8ddac4d3bb3eec3d4865

Added some missing glDisable calls.  Removed unneeded texture-related function calls from RectangleStimulus::drawInUnitSquare.

plugins/core/DriftingGratingStimulus/DriftingGratingStimulus.cpp
@@ -208,6 +208,8 @@ void DriftingGratingStimulus::load(shared_ptr<StimulusDisplay> display) {
 		glTexEnvi(GL_TEXTURE_ENV, GL_SOURCE0_ALPHA_ARB, GL_TEXTURE);
 		glTexEnvi(GL_TEXTURE_ENV, GL_OPERAND0_ALPHA_ARB, GL_SRC_ALPHA);
         
+        glDisable(GL_BLEND);
+        
         // Unbind grating texture
         glActiveTextureARB(GL_TEXTURE0_ARB); 
         glBindTexture( GL_TEXTURE_1D, 0 );
@@ -384,6 +386,8 @@ void DriftingGratingStimulus::drawFrame(shared_ptr<StimulusDisplay> display) {
     // ----------------------------------------
     //                CLEAN-UP
     // ----------------------------------------
+    
+    glDisable(GL_BLEND);
     
     // Unbind grating texture
     glActiveTextureARB(GL_TEXTURE0_ARB); 

core/Core/Stimuli/StandardStimuli.cpp
@@ -491,7 +491,9 @@ void ImageStimulus::drawInUnitSquare(shared_ptr<StimulusDisplay> display) {
         glEnd();
 		
 		
-		glBindTexture(GL_TEXTURE_2D, 0); // unbind that fucker
+        glDisable(GL_BLEND);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glDisable(GL_TEXTURE_2D);
         
         last_alpha = a;
 		
@@ -562,19 +564,15 @@ void RectangleStimulus::drawInUnitSquare(shared_ptr<StimulusDisplay> display) {
 	GLfloat _g = (float)(*g);
 	GLfloat _b = (float)(*b);
 	GLfloat _a = (float)(*alpha_multiplier);
-	   
-	glBindTexture(GL_TEXTURE_2D, 0);
+    
+    glColor4f(_r, _g, _b, _a);
+	  
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	glEnable (GL_BLEND); 
 	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_ADD);
 	
     glBegin(GL_QUADS);
 		
-		//mprintf("fixpoint r: %g, g: %g, b: %g", red, green, blue);
-	
-        //glColor3f(_r,_g,_b);
-		glColor4f(_r, _g, _b, _a);
         glVertex3f(0.0,0.0,0.0);
 		glVertex3f(1.0,0.0,0.0);
         glVertex3f(1.0,1.0,0.0);

commit 1ded8f0a891c3030084ff41810bc391801b0ab6b

Fixed drawing of gratings with alpha multiplier less than one (broken by commit b6dfa1f)

plugins/core/DriftingGratingStimulus/DriftingGratingStimulus.cpp
@@ -208,8 +208,6 @@ void DriftingGratingStimulus::load(shared_ptr<StimulusDisplay> display) {
 		glTexEnvi(GL_TEXTURE_ENV, GL_SOURCE0_ALPHA_ARB, GL_TEXTURE);
 		glTexEnvi(GL_TEXTURE_ENV, GL_OPERAND0_ALPHA_ARB, GL_SRC_ALPHA);
         
-        glDisable(GL_BLEND);
-        
         // Unbind grating texture
         glActiveTextureARB(GL_TEXTURE0_ARB); 
         glBindTexture( GL_TEXTURE_1D, 0 );
@@ -225,6 +223,8 @@ void DriftingGratingStimulus::load(shared_ptr<StimulusDisplay> display) {
         glDisable(GL_TEXTURE_2D);
         
         glActiveTextureARB(0);
+        
+        glDisable(GL_BLEND);
 
 	}
     
@@ -387,8 +387,6 @@ void DriftingGratingStimulus::drawFrame(shared_ptr<StimulusDisplay> display) {
     //                CLEAN-UP
     // ----------------------------------------
     
-    glDisable(GL_BLEND);
-    
     // Unbind grating texture
     glActiveTextureARB(GL_TEXTURE0_ARB); 
     glBindTexture( GL_TEXTURE_1D, 0 );
@@ -421,8 +419,9 @@ void DriftingGratingStimulus::drawFrame(shared_ptr<StimulusDisplay> display) {
         glEnd();  // GL_QUADS
     }
     
-
-	glPopMatrix();	
+    
+    glDisable(GL_BLEND);
+	glPopMatrix();
 }
 
 