e7461ecb785d76c3a84bb9f9f56e2842f4c74049
Christopher Stawarz <cstawarz@mit.edu>
2016-09-23 15:51:25 -0400

    Added DynamicStimulusBase template class, which performs the same functions as StandardDynamicStimulus but takes the base stimulus type as a template parameter.  StandardDynamicStimulus is now an instantiation of DynamicStimulusBase (using Stimulus as the base stimulus type).

--- core/Core/Stimuli/DynamicStimuli/StandardDynamicStimulus.h
+++ core/Core/Stimuli/DynamicStimuli/StandardDynamicStimulus.h
@@ -18,14 +18,15 @@
 BEGIN_NAMESPACE_MW
 
 
-class StandardDynamicStimulus : public Stimulus, public DynamicStimulusDriver {
+template<typename BaseStimulus>
+class DynamicStimulusBase : public BaseStimulus, public DynamicStimulusDriver {
     
 public:
     static const std::string AUTOPLAY;
     
     static void describeComponent(ComponentInfo &info);
 
-    explicit StandardDynamicStimulus(const ParameterValueMap &parameters);
+    explicit DynamicStimulusBase(const ParameterValueMap &parameters);
     
     void setVisible(bool newvis) override;
     bool needDraw() override;
@@ -42,6 +43,81 @@ private:
     bool didDrawWhilePaused;
     
 };
+
+
+template<typename BaseStimulus>
+const std::string DynamicStimulusBase<BaseStimulus>::AUTOPLAY("autoplay");
+
+
+template<typename BaseStimulus>
+void DynamicStimulusBase<BaseStimulus>::describeComponent(ComponentInfo &info) {
+    BaseStimulus::describeComponent(info);
+    info.addParameter(AUTOPLAY, "NO");
+}
+
+
+template<typename BaseStimulus>
+DynamicStimulusBase<BaseStimulus>::DynamicStimulusBase(const ParameterValueMap &parameters) :
+    BaseStimulus(parameters),
+    autoplay(parameters[AUTOPLAY]),
+    didDrawWhilePaused(false)
+{ }
+
+
+template<typename BaseStimulus>
+void DynamicStimulusBase<BaseStimulus>::setVisible(bool newvis) {
+    boost::mutex::scoped_lock locker(stim_lock);
+    
+    BaseStimulus::setVisible(newvis);
+    
+    if (!newvis && isPlaying() && autoplay->getValue().getBool()) {
+        stopPlaying();
+    }
+}
+
+
+template<typename BaseStimulus>
+bool DynamicStimulusBase<BaseStimulus>::needDraw() {
+    return isPlaying() && !(isPaused() && didDrawWhilePaused);
+}
+
+
+template<typename BaseStimulus>
+void DynamicStimulusBase<BaseStimulus>::draw(shared_ptr<StimulusDisplay> display) {
+    boost::mutex::scoped_lock locker(stim_lock);
+    
+    if (!isPlaying()) {
+        if (autoplay->getValue().getBool()) {
+            startPlaying();
+        } else {
+            return;
+        }
+    }
+    
+    drawFrame(display);
+    
+    if (isPaused()) {
+        didDrawWhilePaused = true;
+    }
+}
+
+
+template<typename BaseStimulus>
+Datum DynamicStimulusBase<BaseStimulus>::getCurrentAnnounceDrawData() {
+    Datum announceData = BaseStimulus::getCurrentAnnounceDrawData();
+    announceData.addElement("start_time", getStartTime());
+    return std::move(announceData);
+}
+
+
+template<typename BaseStimulus>
+void DynamicStimulusBase<BaseStimulus>::beginPause() {
+    DynamicStimulusDriver::beginPause();
+    didDrawWhilePaused = false;
+}
+
+
+using StandardDynamicStimulus = DynamicStimulusBase<Stimulus>;
 
 
 END_NAMESPACE_MW
