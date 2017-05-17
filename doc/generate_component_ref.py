2e3974c52153e30772d9a7869079e8695eff5f1a
Christopher Stawarz <cstawarz@mit.edu>
2017-05-17 15:22:04 -0400

    Handle inheritance of 'allowed_parent', 'transient', and 'toplevel' fields in generate_component_ref.py

--- doc/generate_component_ref.py
+++ doc/generate_component_ref.py
@@ -56,18 +56,25 @@ def register_element(info):
         if alias:
             info['alias'] = alias
         group = set(str_or_list(info.get('group', [])))
+        allowed_parent = set(str_or_list(info.get('allowed_parent', [])))
         platform = list(str_or_list(info.get('platform', [])))
         parameters = info.get('parameters', [])
         param_names = set(p['name'] for p in parameters)
         for ancestor in str_or_list(info.get('isa', [])):
             ancestor = components[ancestor]
             group.update(ancestor['group'])
+            if ('transient' not in info) and ('transient' in ancestor):
+                info['transient'] = ancestor['transient']
+            if ('toplevel' not in info) and ('toplevel' in ancestor):
+                info['toplevel'] = ancestor['toplevel']
+            allowed_parent.update(ancestor['allowed_parent'])
             platform = ancestor.get('platform', []) + platform
             for p in ancestor.get('parameters', []):
                 if p['name'] not in param_names:
                     parameters.append(p)
                     param_names.add(p['name'])
         info['group'] = list(group)  # json.dump chokes on set
+        info['allowed_parent'] = list(allowed_parent)
         if platform:
             info['platform'] = platform
         if parameters:
