c5b2389b473850377bcc709713fc1cc3503546d3
Christopher Stawarz <cstawarz@mit.edu>
2017-12-18 13:15:20 -0500

    Upgraded to zeromq 4.2.3

--- supporting_libs/build_supporting_libs.py
+++ supporting_libs/build_supporting_libs.py
@@ -336,16 +336,14 @@ def boost(ios=True):
 
 @builder
 def zeromq(ios=True):
-    version = '4.1.6'
+    version = '4.2.3'
     srcdir = 'zeromq-' + version
     tarfile = srcdir + '.tar.gz'
 
     with done_file(srcdir):
         if not os.path.isdir(srcdir):
-            download_archive('https://github.com/zeromq/zeromq4-1/releases/download/v%s/' % version, tarfile)
+            download_archive('https://github.com/zeromq/libzmq/releases/download/v%s/' % version, tarfile)
             unpack_tarfile(tarfile, srcdir)
-            with workdir(srcdir):
-                apply_patch('zeromq_clock_gettime.patch')
 
         zeromq_builddir = os.path.join(builddir, 'zeromq')
         make_directory(zeromq_builddir)
