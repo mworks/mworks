941ccbd6c5ad17c1840186dbfc9ac5cbe1171cfe
Christopher Stawarz <cstawarz@mit.edu>
2019-02-28 14:20:13 -0500

    Updated app icons

---
+++ apps/icons/export_icon.py
@@ -0,0 +1,19 @@
+from subprocess import Popen, PIPE
+
+
+acorn_png_export_applescript = '''
+tell application "Acorn"
+    tell document 1
+        resize image width %(size)d
+        save as PNG in POSIX file "%(path)s"
+        undo
+    end tell
+end tell
+'''
+
+
+def export_icon(size, path):
+    cmd = Popen(['/usr/bin/osascript'], stdin=PIPE)
+    cmd.communicate(acorn_png_export_applescript %
+                    {'size': size, 'path': path})
+    assert cmd.returncode == 0
