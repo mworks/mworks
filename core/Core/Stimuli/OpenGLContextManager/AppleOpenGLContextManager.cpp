a981b8556b4cbc6605db15239f9f879c3b2dd406
Christopher Stawarz <cstawarz@mit.edu>
2019-04-02 11:08:41 -0400

    Don't require explicit opt-in for new MacOSOpenGLContextManager, as using LegacyMacOSOpenGLContextManager appears only to mask (not resolve) performance problems on macOS 10.14

--- core/Core/Stimuli/OpenGLContextManager/AppleOpenGLContextManager.cpp
+++ core/Core/Stimuli/OpenGLContextManager/AppleOpenGLContextManager.cpp
@@ -11,11 +11,6 @@
 #if TARGET_OS_OSX
 #  include "LegacyMacOSOpenGLContextManager.h"
 #  include "MacOSOpenGLContextManager.hpp"
-#  define METAL_BACKING_OPT_IN_REQUIRED  1
-#  if METAL_BACKING_OPT_IN_REQUIRED
-#    include "ComponentRegistry.h"
-#    define USE_METAL_BACKING_KEY  "use_metal_backing"
-#  endif
 #elif TARGET_OS_IPHONE
 #  include "IOSOpenGLContextManager.hpp"
 #endif
@@ -122,51 +117,25 @@ auto AppleOpenGLContextManager::getMirrorView() const -> PlatformViewPtr {
 
 
 boost::shared_ptr<OpenGLContextManager> OpenGLContextManager::createPlatformOpenGLContextManager() {
-    @autoreleasepool {
 #if TARGET_OS_OSX
-#  if METAL_BACKING_OPT_IN_REQUIRED
-        if (auto mainScreenInfo = ComponentRegistry::getSharedRegistry()->getVariable(MAIN_SCREEN_INFO_TAGNAME)) {
-            auto value = mainScreenInfo->getValue();
-            if (value.hasKey(USE_METAL_BACKING_KEY) && value.getElement(USE_METAL_BACKING_KEY).getBool()) {
-#  endif
-                // Only use the modern, Metal-backed OpenGLContextManager on macOS 10.13 and later,
-                // as the window server did not use Metal in earlier releases (so presumably OpenGL
-                // is more efficient there)
-                if (@available(macOS 10.13, *)) {
-                    // Check if Metal is supported by attempting to obtain the default device
-                    if (id<MTLDevice> metalDevice = MTLCreateSystemDefaultDevice()) {
-                        [metalDevice release];
-#  if METAL_BACKING_OPT_IN_REQUIRED
-                        mwarning(M_DISPLAY_MESSAGE_DOMAIN,
-                                 "Metal backing of stimulus display is experimental and not recommended "
-                                 "for production use");
-#  endif
-                        return boost::make_shared<MacOSOpenGLContextManager>();
-#  if METAL_BACKING_OPT_IN_REQUIRED
-                    } else {
-                        merror(M_DISPLAY_MESSAGE_DOMAIN,
-                               "Metal is not supported on this system. "
-                               "Using OpenGL backing for stimulus display.");
-#  endif
-                    }
-#  if METAL_BACKING_OPT_IN_REQUIRED
-                } else {
-                    merror(M_DISPLAY_MESSAGE_DOMAIN,
-                           "Metal backing of stimulus display requires macOS 10.13 or later. "
-                           "Using OpenGL backing instead.");
-#  endif
-                }
-#  if METAL_BACKING_OPT_IN_REQUIRED
+    @autoreleasepool {
+        // Only use the modern, Metal-backed OpenGLContextManager on macOS 10.13 and later,
+        // as the window server did not use Metal in earlier releases (so presumably OpenGL
+        // is more efficient there)
+        if (@available(macOS 10.13, *)) {
+            // Check if Metal is supported by attempting to obtain the default device
+            if (id<MTLDevice> metalDevice = MTLCreateSystemDefaultDevice()) {
+                [metalDevice release];
+                return boost::make_shared<MacOSOpenGLContextManager>();
             }
         }
-#  endif
         return boost::make_shared<LegacyMacOSOpenGLContextManager>();
-#elif TARGET_OS_IPHONE
-        return boost::make_shared<IOSOpenGLContextManager>();
-#else
-#  error Unsupported platform
-#endif
     }
+#elif TARGET_OS_IPHONE
+    return boost::make_shared<IOSOpenGLContextManager>();
+#else
+#   error Unsupported platform
+#endif
 }
 
 
