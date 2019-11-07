cfbc956f9a2684b19abbab00b690e955ba3e3846
Christopher Stawarz <cstawarz@mit.edu>
2019-11-07 13:18:15 -0500

    Created an Xcode project for MWEL.  MWEL scripts now invoke a custom, private Python executable (which links against MWorksPython and invokes MWorksPythonMain).

---
+++ mwel/mwpython.c
@@ -0,0 +1,14 @@
+//
+//  mwpython.c
+//  mwpython
+//
+//  Created by Christopher Stawarz on 11/7/19.
+//  Copyright © 2019 The MWorks Project. All rights reserved.
+//
+
+#include <MWorksPython/MWorksPython.h>
+
+
+int main(int argc, const char * argv[]) {
+    return MWorksPythonMain(argc, argv);
+}
