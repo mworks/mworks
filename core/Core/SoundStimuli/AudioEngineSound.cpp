16696029fd4bd54a6aa1e559667bfa6efe8e6521
Christopher Stawarz <cstawarz@mit.edu>
2021-08-03 09:12:24 -0400

    Added AudioEngineSound::EngineManager

--- core/Core/SoundStimuli/AudioEngineSound.cpp
+++ core/Core/SoundStimuli/AudioEngineSound.cpp
@@ -12,8 +12,87 @@ BEGIN_NAMESPACE_MW
 
 
 AudioEngineSound::AudioEngineSound(const ParameterValueMap &parameters) :
-    Sound(parameters)
+    Sound(parameters),
+    engineManager(getEngineManager())
 { }
+
+
+AudioEngineSound::EngineManager::EngineManager() :
+    engine(nil)
+{
+    @autoreleasepool {
+        engine = [[AVAudioEngine alloc] init];
+        [engine prepare];
+    }
+    
+    auto callback = [this](const Datum &data, MWorksTime time) { stateSystemCallback(data, time); };
+    stateSystemCallbackNotification = boost::make_shared<VariableCallbackNotification>(callback);
+    if (auto stateSystemMode = state_system_mode) {
+        stateSystemMode->addNotification(stateSystemCallbackNotification);
+    }
+}
+
+
+AudioEngineSound::EngineManager::~EngineManager() {
+    stateSystemCallbackNotification->remove();
+    
+    @autoreleasepool {
+        if (engine) {
+            if (engine.running) {
+                [engine stop];
+            }
+            engine = nil;
+        }
+    }
+}
+
+
+void AudioEngineSound::EngineManager::stateSystemCallback(const Datum &data, MWorksTime time) {
+    lock_guard lock(mutex);
+    
+    @autoreleasepool {
+        switch (data.getInteger()) {
+            case RUNNING:
+                if (!engine.running) {
+                    NSError *error = nil;
+                    if (![engine startAndReturnError:&error]) {
+                        merror(M_SYSTEM_MESSAGE_DOMAIN,
+                               "Cannot start audio engine: %s",
+                               error.localizedDescription.UTF8String);
+                    }
+                }
+                break;
+                
+            case PAUSED:
+                if (engine.running) {
+                    [engine pause];
+                }
+                break;
+                
+            case IDLE:
+                if (engine.running) {
+                    [engine pause];
+                    [engine reset];
+                }
+                break;
+        }
+    }
+}
+
+
+auto AudioEngineSound::getEngineManager() -> boost::shared_ptr<EngineManager> {
+    static boost::weak_ptr<EngineManager> weakManager;
+    static mutex_type mutex;
+    
+    lock_guard lock(mutex);
+    
+    auto manager = weakManager.lock();
+    if (!manager) {
+        manager = boost::make_shared<EngineManager>();
+        weakManager = manager;
+    }
+    return manager;
+}
 
 
 END_NAMESPACE_MW
--- core/Core/SoundStimuli/AudioEngineSound.hpp
+++ core/Core/SoundStimuli/AudioEngineSound.hpp
@@ -8,6 +8,8 @@
 #ifndef AudioEngineSound_hpp
 #define AudioEngineSound_hpp
 
+#include <AVFoundation/AVAudioEngine.h>
+
 #include <boost/noncopyable.hpp>
 
 #include "Sound.h"
@@ -20,6 +22,32 @@ class AudioEngineSound : public Sound, boost::noncopyable {
     
 public:
     explicit AudioEngineSound(const ParameterValueMap &parameters);
+    
+protected:
+    using mutex_type = std::mutex;
+    using lock_guard = std::lock_guard<mutex_type>;
+    using unique_lock = std::unique_lock<mutex_type>;
+    
+    AVAudioEngine * getEngine(unique_lock &lock) const { return engineManager->getEngine(lock); }
+    
+private:
+    struct EngineManager : boost::noncopyable {
+        EngineManager();
+        ~EngineManager();
+        AVAudioEngine * getEngine(unique_lock &lock) const {
+            lock = unique_lock(mutex);
+            return engine;
+        }
+    private:
+        void stateSystemCallback(const Datum &data, MWorksTime time);
+        AVAudioEngine *engine;
+        boost::shared_ptr<VariableCallbackNotification> stateSystemCallbackNotification;
+        mutable mutex_type mutex;
+    };
+    
+    static boost::shared_ptr<EngineManager> getEngineManager();
+    
+    const boost::shared_ptr<EngineManager> engineManager;
     
 };
 
