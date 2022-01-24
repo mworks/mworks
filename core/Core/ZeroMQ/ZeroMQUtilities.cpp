ec818b67c1b561589d0e71db5f0eda452cacfcf8
Christopher Stawarz <cstawarz@mit.edu>
2022-01-24 11:59:46 -0500

    zeromq::getHostname now attempts to find the "most public" hostname or address but will return a link-local or loopback address if nothing better is available

--- core/Core/ZeroMQ/ZeroMQUtilities.cpp
+++ core/Core/ZeroMQ/ZeroMQUtilities.cpp
@@ -38,30 +38,80 @@ bool getHostname(std::string &hostname) {
             freeifaddrs(interfaces);
         } BOOST_SCOPE_EXIT_END
         
+        //
+        // We want the "most public" hostname or address, so we have to jump through some hoops.  First,
+        // we classify each active IPv4 and IPv6 address as loopback, link local, private, or "default"
+        // (meaning none of the first three classes).  Next, we sort the addresses by class, with
+        // default first, private second, link local third, and loopback last.  Finally, we try to
+        // convert each address in the sorted list to a name or numeric address and return the first one
+        // that succeeds.
+        //
+        
+        enum {
+            DEFAULT,
+            PRIVATE,
+            LINKLOCAL,
+            LOOPBACK
+        };
+        using AddressInfo = std::pair<struct sockaddr *, int>;
+        std::vector<AddressInfo> candidateAddrs;
+        
         for (auto ifa = interfaces; ifa; ifa = ifa->ifa_next) {
-            if ((ifa->ifa_flags & IFF_UP) &&
-                !(ifa->ifa_flags & IFF_LOOPBACK) &&
-                ifa->ifa_addr)
-            {
+            if ((ifa->ifa_flags & IFF_UP) && ifa->ifa_addr) {
                 auto addr = ifa->ifa_addr;
-                if ((addr->sa_family == AF_INET &&
-                     // s_addr is stored in network (i.e. big-endian) byte order, so we must convert it
-                     // to host byte order before using it
-                     !IN_LINKLOCAL(ntohl(reinterpret_cast<struct sockaddr_in *>(addr)->sin_addr.s_addr))) ||
-                    (addr->sa_family == AF_INET6 &&
-                     !IN6_IS_ADDR_LINKLOCAL(&(reinterpret_cast<struct sockaddr_in6 *>(addr)->sin6_addr))))
-                {
-                    std::array<char, NI_MAXHOST> host;
-                    auto error = getnameinfo(addr, addr->sa_len, host.data(), host.size(), nullptr, 0, 0);
-                    if (0 == error) {
-                        hostname = host.data();
-                        return true;
+                auto addrType = DEFAULT;
+                
+                switch (addr->sa_family) {
+                    case AF_INET: {
+                        // s_addr is stored in network (i.e. big-endian) byte order, so we must convert it
+                        // to host byte order before using it
+                        auto s_addr = ntohl(reinterpret_cast<struct sockaddr_in *>(addr)->sin_addr.s_addr);
+                        if (IN_LOOPBACK(s_addr)) {
+                            addrType = LOOPBACK;
+                        } else if (IN_LINKLOCAL(s_addr)) {
+                            addrType = LINKLOCAL;
+                        } else if (IN_PRIVATE(s_addr)) {
+                            addrType = PRIVATE;
+                        }
+                        break;
                     }
-                    merror(M_NETWORK_MESSAGE_DOMAIN,
-                           "Cannot convert socket address to host name: %s",
-                           gai_strerror(error));
+                        
+                    case AF_INET6: {
+                        auto sin6_addr = &(reinterpret_cast<struct sockaddr_in6 *>(addr)->sin6_addr);
+                        if (IN6_IS_ADDR_LOOPBACK(sin6_addr)) {
+                            addrType = LOOPBACK;
+                        } else if (IN6_IS_ADDR_LINKLOCAL(sin6_addr)) {
+                            addrType = LINKLOCAL;
+                        } else if (IN6_IS_ADDR_SITELOCAL(sin6_addr) || IN6_IS_ADDR_UNIQUE_LOCAL(sin6_addr)) {
+                            addrType = PRIVATE;
+                        }
+                        break;
+                    }
+                        
+                    default:
+                        continue;
                 }
+                
+                candidateAddrs.emplace_back(addr, addrType);
             }
+        }
+        
+        // Use stable_sort to preserve the original ordering of addresses within each type
+        std::stable_sort(candidateAddrs.begin(), candidateAddrs.end(), [](const AddressInfo &a, const AddressInfo &b) {
+            return (a.second < b.second);
+        });
+        
+        for (const auto &info : candidateAddrs) {
+            auto &addr = info.first;
+            std::array<char, NI_MAXHOST> host;
+            auto error = getnameinfo(addr, addr->sa_len, host.data(), host.size(), nullptr, 0, 0);
+            if (0 == error) {
+                hostname = host.data();
+                return true;
+            }
+            merror(M_NETWORK_MESSAGE_DOMAIN,
+                   "Cannot convert socket address to host name: %s",
+                   gai_strerror(error));
         }
     }
     
@@ -114,30 +164,3 @@ bool resolveHostname(const std::string &hostname, std::string &address) {
 
 END_NAMESPACE(zeromq)
 END_NAMESPACE_MW
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
